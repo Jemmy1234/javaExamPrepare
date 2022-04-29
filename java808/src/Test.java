7df515bfc068aa5716129f159716a3faf112ecb7
Jemmy <jemmy@xxentria-tech.com>
2022-04-29 17:03:24 +0800

    Update to Q95

--- java808/src/Test.java
+++ java808/src/Test.java
@@ -11,16 +11,6 @@ public class Test {
 
     public static void main(String[] args) {
 
-        String arra[][] = new String[3][];
-        arra[0] = new String[] { "rose", "lily" };
-        arra[1] = new String[] { "apple", "berry", "cherry", "grapes" };
-        arra[0] = new String[] { "beans", "carrot", "potato" };
-        // insert
-
-        for (String a[]:arra[][]) { 
-            for (String x:a[]) { 
-                toUpperCase(); 
-            } 
-        }
+        
     }
 }
