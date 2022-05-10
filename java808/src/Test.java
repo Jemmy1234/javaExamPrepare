ff13175283de22df800331369d1306adeeb437c4
Jemmy <jemmy@xxentria-tech.com>
2022-05-10 09:27:44 +0800

    Update to Q161

--- java808/src/Test.java
+++ java808/src/Test.java
@@ -7,27 +7,11 @@ public class Test {
 
     public static void main(String[] args) {
 
-        List cs = new ArrayList();
-        cs.add("Java");
-        cs.add("C");
-        
-        Student s = new Student(123, "Fred", cs);
-        System.out.println(s);
-    }
-}
-
-class Student {
-    int rollnumber;
-    String name;
-    List cources = new ArrayList();
-
-    // insert code here
-    Student(int rollnumber , String name, ArrayList cources) {
-        this.cources = cources;
-    }
-
-    public String toString() {
-        return rollnumber + " : " + name + " : " + cources;
+        short a = 200;
+        long b = (long)a;
+        int c = (int) b;
+        long d = (long)c;
+        String e = String.valueOf(d);
     }
 }
 
